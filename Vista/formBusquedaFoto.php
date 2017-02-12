c2b8d5c66401dd73b685b0384ae0abb764cdef74
ander <anderh94@gmail.com>
2017-02-12 17:22:05 +0100

    Cambios v2

---
+++ Vista/formBusquedaFoto.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: ander
+ * Date: 12/02/17
+ * Time: 16:41
+ */
+abstract class formBusquedaFoto
+{
+
+    public static function vBFoto(){
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+}
